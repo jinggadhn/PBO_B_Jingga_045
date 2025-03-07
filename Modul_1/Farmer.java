3a3b7a604477120bb36f9bdfa573edecd9712b4a
jinggadhn <jinggadhina4@gmail.com>
2025-03-07 08:06:14 +0700

    third

---
+++ Modul_1/Farmer.java
@@ -0,0 +1,8 @@
+public class Farmer {
+    String name;
+    String favourite;
+    void talk(){
+        System.out.println("Hi! My name is: " + name + ". My favorite plant is: " + favourite);
+        System.out.println();
+        }
+}
